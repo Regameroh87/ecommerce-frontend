d6e7a2854d4cec3263150d84a5d6f863ec3c439f
Regameroh87 <gamero.rodrigo@gmail.com>
2025-05-22 13:46:43 -0300

    agregando cambiso al chatbot

--- src/app/api/actions.ts
+++ src/app/api/actions.ts
@@ -4,12 +4,23 @@ import OpenAI from "openai";
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+console.log("API Key: ", process.env.OPENAI_API_KEY);
 
 export async function getChatGptResponse(prompt: string) {
   const response = await openai.chat.completions
     .create({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt }],
+      model: "gpt-4",
+      messages: [
+        {
+          role: "system",
+          content:
+            "Eres un entrenador personal experto en fitness y nutrición. Responde de manera motivadora y profesional.",
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
     })
     .then((res) => {
       return res.choices[0].message.content;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,50 @@
 "use client";
+import React from "react";
 import { useState } from "react";
 import { getChatGptResponse } from "./api/actions";
 
 export default function Home() {
   const [prompt, setPrompt] = useState("");
+  type Message = { role: string; content: string };
+  const [chat, setChat] = useState<Message[]>([]);
 
   function onChange(e: React.ChangeEvent<HTMLInputElement>) {
     setPrompt(() => e.target.value);
   }
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     console.log("prompt: ", prompt);
-    const response = getChatGptResponse(prompt);
+    setChat((prev) => [...prev, { role: "user", content: prompt }]);
+    setPrompt("");
+    const response = await getChatGptResponse(prompt);
     console.log("response: ", response);
+    if (response) {
+      setChat((prev) => [...prev, { role: "assistant", content: response }]);
+    }
   }
+
   return (
     <div className="flex p-80 justify-center min-h-screen">
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-4">
+          {chat?.map((message, index) => (
+            <div
+              key={index}
+              className={`${
+                message.role === "user" ? "text-blue-500" : "text-green-500"
+              } bg-gray-200 p-2 rounded-2xl`}
+            >
+              {message.content}
+            </div>
+          ))}
+        </div>
         <input
           className=" bg-white p-2 text-black"
           type="text"
           name="message"
+          autoComplete="off"
+          value={prompt}
           onChange={onChange}
         />
         <button
