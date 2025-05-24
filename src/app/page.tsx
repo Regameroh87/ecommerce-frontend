c4eba5ce2e541098bdf1ae6e992b5dca7cc5df81
Regameroh87 <gamero.rodrigo@gmail.com>
2025-05-24 11:57:29 -0300

    refactor: extrae la visualización de mensajes del chat a un componente reutilizable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ import { useState } from "react";
 export default function Home() {
   const [prompt, setPrompt] = useState("");
   type Message = { role: string; content: string };
-  const [chat, setChat] = useState<Message[]>([]);
+  const [partialMessage, setPartialMessage] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isStreaming, setIsStreaming] = useState(false);
 
   function onChange(e: React.ChangeEvent<HTMLInputElement>) {
     setPrompt(() => e.target.value);
@@ -15,9 +17,9 @@ export default function Home() {
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     console.log("prompt: ", prompt);
-    setChat((prev) => [...prev, { role: "user", content: prompt }]);
+    setMessages((prev) => [...prev, { role: "user", content: prompt }]);
     setPrompt("");
-    /* const response = await getChatGptResponse(prompt); */
+
     const response = await fetch("http://localhost:3000/api/chat", {
       method: "POST",
       headers: {
@@ -27,27 +29,33 @@ export default function Home() {
     });
     const reader = response.body?.getReader();
 
-    while (true) {
-      const { done, value } = await reader!.read();
-      if (done) break;
-      const text = new TextDecoder("utf-8").decode(value);
-      setChat((prev) => [...prev, { role: "assistant", content: text }]);
+    if (reader) {
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        const text = new TextDecoder("utf-8").decode(value);
+        /* setChat((prev) => [...prev, { role: "assistant", content: text }]); */
+      }
     }
   }
   return (
     <div className="flex p-80 justify-center min-h-screen">
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-4">
-          {chat?.map((message, index) => (
+        <div className="chat-window">
+          {messages.map((msg, i) => (
             <div
-              key={index}
-              className={`${
-                message.role === "user" ? "text-blue-500" : "text-green-500"
-              } bg-gray-200 p-2 rounded-2xl`}
+              key={i}
+              className={msg.sender === "user" ? "text-right" : "text-left"}
             >
-              {message.content}
+              {msg.text}
             </div>
           ))}
+
+          {isStreaming && (
+            <div className="text-left text-gray-500 italic">
+              {partialMessage}
+            </div>
+          )}
         </div>
         <input
           className=" bg-white p-2 text-black"
